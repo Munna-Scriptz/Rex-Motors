90d4ca53f5b25e257942ab214176ce8140c74d6a
Munna-Scripts <munnascriptz@gmail.com>
2025-10-18 22:43:07 +0600

    Listing Complete

---
+++ src/components/common/SingleListing.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Link } from 'react-router'
+import car from '../../assets/singleLIst.jpg'
+import { MdArrowOutward } from 'react-icons/md'
+import { FaCheck } from 'react-icons/fa6'
+import { CiBookmark } from 'react-icons/ci'
+import { IoSpeedometerOutline } from 'react-icons/io5'
+import { BsFuelPumpDiesel } from 'react-icons/bs'
+import { PiPathDuotone } from 'react-icons/pi'
+
+const SingleListing = () => {
+  return (
+    <div className='w-[360px] mx-auto rounded-[24px] py-6 px-6 border-1 border-[#D8D8D8]'>
+
+
+        {/* ---------------------- Image section  */}
+        <div className='relative '>
+            <img className='rounded-[12px]' src={car} alt="Car image" />
+
+            <div className='absolute left-[15px] top-[15px] w-[120px] bg-[#3D923A] px-3 py-1.5 rounded-[8px] flex items-center gap-2'>
+                <div className='bg-white rounded-full flex items-center justify-center w-[16px] h-[16px] text-xs'>
+                    <FaCheck />
+                </div>
+                <p className='text-white font-bold text-xs'>Great Price</p>
+            </div>
+
+            {/* ------- Save button  */}
+            <div className='bg-white absolute top-2.5 right-2.5 rounded-full flex items-center justify-center w-[35px] h-[35px] text-lg text-brand cursor-pointer hover:bg-primary hover:text-white hover:scale-[1.05] duration-300'>
+                <CiBookmark />
+            </div>
+
+        </div>
+
+        {/* --------------------- Texts  */}
+        <div className='mt-7 border-b pb-2 border-[#E9E9E9]'>
+            <h2 className='text-[#050B20] text-xl font-semibold mt-4 w-[340px]'>T-Cross - 2023</h2>
+            <p className='text-second text-base w-[340px]'>4.0 D5 PowerPulse Momentum 5dr</p>
+        </div>
+
+        {/* --------------------- Abilities  */}
+        <div className='mt-7 border-b pb-2 border-[#E9E9E9] flex items-center justify-between'>
+            <div className='flex items-center justify-center flex-col gap-2'>
+                <IoSpeedometerOutline className='text-[26px]' />
+                <p className='text-primary'>1500 Miles</p>
+            </div>
+            <div className='flex items-center justify-center flex-col gap-2'>
+                <BsFuelPumpDiesel className='text-[26px]' />
+                <p className='text-primary'>Diesel</p>
+            </div>
+            <div className='flex items-center justify-center flex-col gap-2'>
+                <PiPathDuotone className='text-[26px]' />
+                <p className='text-primary'>CVT</p>
+            </div>
+        </div>
+
+        {/* -------------------- Price  */}
+        <div className='mt-6 flex items-center justify-between'>
+            <h2 className='text-[#3D923A] text-xl font-bold'>$82,000</h2>
+        <Link to={'/'} className='bg-white rounded-full px-4 py-1 w-fit text-second text-base font-bold hover:bg-second hover:text-white duration-300 flex items-center gap-3'>
+            View All
+            <div className='w-[24px] h-[24px] bg-brand text-white rounded-full flex items-center justify-center text-lg'><MdArrowOutward className=''/></div>
+        </Link>
+        </div>
+    </div>
+  )
+}
+
+export default SingleListing
--- src/components/home/Listing.jsx
+++ src/components/home/Listing.jsx
@@ -1,14 +1,58 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import CommonHead from '../common/CommonHead'
+import SingleListing from '../common/SingleListing'
+import Slider from 'react-slick';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa6';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 const Listing = () => {
+    const settings = {
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+    } 
+    const sliderRef = useRef(null);
+
   return (
     <>
         <main id='Listing' className='mt-[112px]'>
             <div className="container">
                 <div id="Listing-Row">
                     {/* ---------------------- Common head  */}
-                    <CommonHead text={'Featured Listings'} />
+                    <CommonHead text={'Featured Listings'} linkName={'View All'} />
+                    <div className='mt-13 flex items-center gap-10'>
+                        <button className='text-primary font-medium border-b border-[#405FF2] pb-2 cursor-pointer'>In Stock</button>
+                        <button className='text-primary font-medium pb-2 cursor-pointer'>New Cars</button>
+                        <button className='text-primary font-medium pb-2 cursor-pointer'>Used Cars</button>
+                    </div>
+
+                    {/* ------------------------ Slider  */}
+                    <div className='mt-10'>
+                        <Slider ref={sliderRef} {...settings}>
+                        <SingleListing />
+                        <SingleListing />
+                        <SingleListing />
+                        <SingleListing />
+
+                        </Slider>
+                    </div>
+                    {/* ----------------Arrow And Dots--------------------- */}
+                    <div className='mt-10 flex items-center justify-center'>
+                        <div className='flex items-center gap-45'>
+                            <div className='text-primary cursor-pointer z-10 w-[30px] h-[30px] hover:bg-brand rounded-full duration-300 hover:text-white flex items-center justify-center' onClick={() => sliderRef.current?.slickPrev()}>
+                                <IoIosArrowBack />
+                            </div>
+
+
+                            <div className='text-primary cursor-pointer z-10 w-[30px] h-[30px] hover:bg-brand rounded-full duration-300 hover:text-white flex items-center justify-center' onClick={() => sliderRef.current?.slickNext()}>
+                                <IoIosArrowForward />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
